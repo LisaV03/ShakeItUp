ab4b84b9add92484b1aeb7a6b3b9d23376c5d52a
Lisa <lisa.veillat@gmail.com>
2023-11-07 19:57:59 +0100

    :sparkles: Add the tab bar

--- app/src/main/java/com/example/shakeitup/MainActivity.kt
+++ app/src/main/java/com/example/shakeitup/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.shakeitup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.shakeitup.ui.categories.CategoriesFragment
+import com.example.shakeitup.ui.ingredients.IngredientsFragment
+import com.example.shakeitup.ui.search.SearchFragment
+import com.google.android.material.tabs.TabLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
+
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tab_layout)
+
+        tabLayout.addOnTabSelectedListener(this)
+        displayCategoriesFragment();
+    }
+
+    fun displayCategoriesFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, CategoriesFragment.newInstance())
+            .commit()
+    }
+
+    fun displaySearchFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, SearchFragment.newInstance())
+            .commit()
+    }
+
+    fun displayIngredientsFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, IngredientsFragment.newInstance())
+            .commit()
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        Log.i("Tab", "onTabSelected");
+        tab?.let {
+            when (tab.position) { // comme un switch case
+                0 -> displaySearchFragment()
+                1 -> displayCategoriesFragment()
+                2 -> displayIngredientsFragment()
+            }
+        }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
     }
 }
