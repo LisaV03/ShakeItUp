f855be80929625fd5efe3771c2f657c972275b46
VOVARD Marine <Marine.Vovard@bordeaux-inp.fr>
2023-11-21 15:30:24 +0100

    :lipstick: Add UI for toolbar

--- app/src/main/java/com/example/shakeitup/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/shakeitup/ui/search/SearchFragment.kt
@@ -18,17 +18,6 @@ import com.example.shakeitup.R
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    //private var param1: String? = null
-    //private var param2: String? = null
-
-    //override fun onCreate(savedInstanceState: Bundle?) {
-      //  super.onCreate(savedInstanceState)
-        //arguments?.let {
-            //param1 = it.getString(ARG_PARAM1)
-            //param2 = it.getString(ARG_PARAM2)
-       // }
-    //}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
